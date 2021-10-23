5e765cecaa49bf650db3ed1b1d5dd9c3c9bdf78f
FlorianLebrun <florian.lebrun>
2021-10-23 13:30:25 +0200

    llvm 12

--- packages/llvm-jit-package/resources/llvm-jit-config.cmake
+++ packages/llvm-jit-package/resources/llvm-jit-config.cmake
@@ -2,4 +2,4 @@
 add_library(llvm-jit INTERFACE)
 set_target_properties(llvm-jit PROPERTIES FOLDER "ThirdParty")
 target_include_directories(llvm-jit INTERFACE "${CMAKE_CURRENT_LIST_DIR}/include")
-target_link_libraries(llvm-jit INTERFACE "${CMAKE_CURRENT_LIST_DIR}/lib/llvm-jit.lib")
+target_link_libraries(llvm-jit INTERFACE "${CMAKE_CURRENT_LIST_DIR}/lib/llvm-jit-$<IF:$<CONFIG:Debug>,debug,release>.lib")
