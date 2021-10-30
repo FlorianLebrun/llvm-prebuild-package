80eb2374fb2722cff4c248ca4f8e813487e66841
FlorianLebrun <florian.lebrun>
2021-10-30 12:30:08 +0200

    chore

--- cmake-2019-x64.bat
+++ cmake-2019-x64.bat
@@ -1,7 +1,8 @@
 set GENERATE_OPTIONS= -G "Visual Studio 16 2019" -A x64
 set GENERATE_PROJECT_NAME=.build-2019-x64
+set GENERATE_DEF=-D PROJECT_NO_NPM_PACK=ON
 
 cd %~dp0
 mkdir %GENERATE_PROJECT_NAME%
 cd %GENERATE_PROJECT_NAME%
-call cmake %GENERATE_OPTIONS% %* -D "PROJECT_DEV_DIR=%~dp0.output" -D "DELIVERY_OUTPUT_DIR=%~dp0.output/delivery" "%~dp0"
+call cmake %GENERATE_OPTIONS% %GENERATE_DEF% %* -D "PROJECT_DEV_DIR=%~dp0.output" -D "DELIVERY_OUTPUT_DIR=%~dp0.output/delivery" "%~dp0"
